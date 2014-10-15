20ee593787e859f104d62a037a2a6f36ef8d679d
Ben Dudgeon <bendudgeon@EwertCS06-2.local>
2014-10-15 14:44:21 +0100

    Added GPIO class

---
+++ GPIOControl.cpp
@@ -0,0 +1,90 @@
+
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "GPIOControl.h"
+
+using namespace std;
+
+GPIOControl::GPIOControl()
+{
+	string GPIONumber = "4";
+}
+
+GPIOControl::GPIOControl(string pinNumber)
+{
+	GPIONumber = pinNumber;
+}
+void GPIOControl::exportGPIO ()
+{
+	string exportPath = "/sys/class/gpio/export";
+	ofstream exportFile;
+	exportFile.open(exportPath.c_str());
+	if (exportFile.is_open())
+	{
+		exportFile << GPIONumber;
+		exportFile.close();
+	}
+	else cout << "Couldn't open GPIO" << GPIONumber << " export file." << endl;
+}
+
+void GPIOControl::unexportGPIO ()
+{
+	string unexportPath = "/sys/class/gpio/unexport";
+	ofstream unexportFile;
+	unexportFile.open(unexportPath.c_str());
+	if (unexportFile.is_open())
+	{
+		unexportFile << GPIONumber;
+		unexportFile.close();
+	}
+	else cout << "Couldn't open GPIO" << GPIONumber << " unexport file." << endl;
+}
+
+void GPIOControl::setDirection (string direction)
+{
+	string directionPath = "/sys/class/gpio/gpio" + GPIONumber + "/direction";
+	ofstream directionFile;
+	directionFile.open(directionPath.c_str());
+	if (directionFile.is_open())
+	{
+		directionFile << direction;
+		directionFile.close();
+	}
+	else cout << "Couldn't open GPIO" << GPIONumber << " direction file." << endl;
+	
+}
+
+void GPIOControlsetValue (string value)
+{
+	string valuePath = "/sys/class/gpio/gpio" + GPIONumber + "/value";
+	ofstream valueFile;
+	valueFile.open(valuePath.c_str());
+	if (valueFile.is_open())
+	{
+		valueFile << value;
+		valueFile.close();
+	}
+	else cout << "Couldn't open GPIO" << GPIONumber << " value file." << endl;
+}
+
+string GPIOControl::getValue ()
+{
+	string value;
+	string valuePath = "/sys/class/gpio/gpio" + GPIONumber + "/value";
+	ifstream valueFile;
+	valueFile.open(valuePath.c_str());
+	if (valueFile.is_open())
+	{
+		getline(valueFile, value);
+		if (value != "0")	value = "1";
+		else 				value = "0";
+		return value;
+	}
+	else
+	{ 
+		cout << "Couldn't open GPIO" << GPIONumber << " value file." << endl;
+		return "0";
+	}
+}
---
+++ GPIOControl.h
@@ -0,0 +1,16 @@
+
+using namespace std;
+class GPIOControl
+{
+	private:
+		string GPIONumber;
+	
+	public:
+		GPIOControl();
+		GPIOControl(string pinNumber);
+		void exportGPIO ();
+		void unexportGPIO ();
+		void setDirection (string direction);
+		void setValue (string value);
+		string getValue ();
+};
